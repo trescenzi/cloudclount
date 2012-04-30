64fb0aabdba54fe436024aba5f6e86d7c07eb497
Ian Wills <urafatrodent@gmail.com>
2012-04-30 00:09:07 -0400

    changed library info

--- src/cloud/count/EBAuditTrailTableModel.java
+++ src/cloud/count/EBAuditTrailTableModel.java
@@ -1,9 +1,17 @@
 package cloud.count;
 
+import badm.Budget;
+import badm.Audit;
+import cc.test.bridge.BridgeConstants;
+import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
 public class EBAuditTrailTableModel extends AbstractTableModel
 {
+    Budget budget; 
+    ArrayList<Audit> audits;
+    BridgeConstants.Side side;
+    
     private String[] columnNames = 
     {
         "Updated", 
@@ -28,7 +36,16 @@ public class EBAuditTrailTableModel extends AbstractTableModel
     @Override
     public int getRowCount() 
     {
-        return 4;
+        if(audits == null && budget != null)
+        {
+            audits = budget.fetchAudits();
+        }
+        else if(budget == null)
+        {
+            return 0;
+        }
+        System.out.println("NUMBER OF NOTES:"+audits.size()+side);
+        return audits.size();
     }
 
     @Override
@@ -40,11 +57,41 @@ public class EBAuditTrailTableModel extends AbstractTableModel
     @Override
     public Object getValueAt(int row, int column) 
     {
+        Audit audit = (Audit) audits.get(row);
+        if(column == 0)
+            ;//return line.getDate();
+        else if(column == 1)
+            ;//return line.getAuthor();
+        else if(column == 2)
+            return audit.getDescription();
+        
         return auditTrailData[column];
     }
     
     public void refresh()
     {
+        if(audits != null){
+            audits.clear();
+            audits = budget.fetchAudits();
+        }
         this.fireTableDataChanged(); // Tells our table the data has changed
+    }
+    
+    public Boolean isNew(int row) 
+    {
+        // Apply Conditions for when a row is new
+        int id = -1;
+        
+        id  = Integer.parseInt(auditTrailData[0]);
+        
+        return id % 2 == 0;
+    }
+    
+    public void setBudget(Budget b){
+        budget = b;
+    }
+    
+    public void setSide(BridgeConstants.Side s){
+        side = s;
     }
 }
